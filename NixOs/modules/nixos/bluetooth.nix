223546b7eb713de85fc8eb9df53a183fc7b7e353
BlueberryBuns <hulewicz.k@gmail.com>
2024-04-06 23:18:39 +0200

    Initial Bluetooth module

---
+++ NixOs/modules/nixos/bluetooth.nix
@@ -0,0 +1,13 @@
+{ pkgs, config, lib, ... }:
+
+{
+  # Bluetooth
+  options = {
+    bluetooth.enable = lib.mkEnableOption "enable bluetooth"
+  };
+
+  config = lib.mkIf config.bluetooth.enable {
+    hardware.bluetooth.enable = true;
+    services.blueman.enable = true;
+  };
+}
